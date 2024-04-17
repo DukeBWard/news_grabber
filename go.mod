f5027bfa61b06c9e2c08d902c0bb966793232891
Luke Ward <lbw4459@rit.edu>
2024-04-17 12:23:03 -0400

    got basic server listening going with chi

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.22.1
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/go-chi/chi v1.5.5 // indirect
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
+// go mod tidy updates the go.mod file with all currently used dependencies
+// go mod init is used when making an new module to make the go.mod file
+// go mod vendor makes the vendor dir in workspace
+// good for a self-contaned project and includes all dependencies
+
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not defined in env")
+	}
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	// print format like C
+	log.Printf("Server starting on port %v", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("PORT:", portString)
