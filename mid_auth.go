b48da8133d4e634e9fb9a31b2845d81e569eaee0
Luke Ward <lbw4459@rit.edu>
2024-05-02 12:32:58 -0400

    config stuff

--- mid_auth.go
+++ mid_auth.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/dukebward/news_grabber/internal/auth"
 	"github.com/dukebward/news_grabber/internal/database"
 )
 
 type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
-func (cfg)
+func (cfg *apiConfig) midAuth(handler authedHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKey, err := auth.GetAPIKey(r.Header)
+		if err != nil {
+			respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+			return
+		}
+
+		// this is enabled because queries is allowed to use it, aka the receiver
+		// every http request has a context on it, make sure to use the context on any calls in handler that require context
+		user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+		if err != nil {
+			respondWithError(w, 400, fmt.Sprintf("Couldn't get user from API: %v", err))
+			return
+		}
+
+		respondWithJSON(w, 200, databaseUserToUser(user))
+	}
+}
