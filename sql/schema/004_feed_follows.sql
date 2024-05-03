f74af1bf85519fa2e494aa3c5a768e7c394de420
Luke Ward <lbw4459@rit.edu>
2024-05-03 12:22:46 -0400

    readme update

--- sql/schema/004_feed_follows.sql
+++ sql/schema/004_feed_follows.sql
@@ -5,7 +5,10 @@ CREATE TABLE feed_follows (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    user_id NOT NULL
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+    -- this combination needs to be unique
+    UNIQUE(user_id, feed_id)
 );
 
 -- +goose Down
